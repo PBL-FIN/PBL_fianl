986fc145b79c0407bfe141f1b76d291ce61568fe
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-15 00:11:28 +0900

    Update problem.java

--- problem/problem.java
+++ problem/problem.java
@@ -1,4 +1,9 @@
 public class Problem {
     private String problem;
     private int answer;
+
+    public Problem(String problem, int answer) {
+        this.problem = problem;
+        this.answer = answer;
+    }
 }
