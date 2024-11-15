67d452237e08cf76f2ea4013526f5956eacb0f38
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-15 21:41:30 +0900

    Update startPage.java

--- page/startPage.java
+++ page/startPage.java
@@ -3,4 +3,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StartPage extends JFrame {}
+public class StartPage extends JFrame {
+  
+    private JButton startButton;
+    private JButton endButton;
+    private JLabel gamePanel;
+
+    public StartPage () {
+        setTitle("도형 넓이 구하는 게임!");
+        setSize(1280, 960);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);  
+}
