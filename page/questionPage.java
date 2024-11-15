1205e5a7a11645f5fe4486803282d76490e14786
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-15 22:47:25 +0900

    Update questionPage.java

--- page/questionPage.java
+++ page/questionPage.java
@@ -7,5 +7,14 @@ public class questionPage extends JFrame {
     private JLabel questionLabel;
     private JButton yesButton;
     private JButton noButton;
-  
+    
+    public questionPage() {
+        setTitle("넓이 구하는 게임");
+        setSize(1280, 960);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null); // FlowLayout 사용
+
+        questionLabel = new JLabel("도형의 넓이 구하는 공식을 아시나요?");
+        yesButton = new JButton("네");
+        noButton = new JButton("아니오");  
 }
