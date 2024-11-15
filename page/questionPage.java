de8975b4ffa681ceda160627b06dc71e02793cf3
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-15 22:48:10 +0900

    Update questionPage.java

--- page/questionPage.java
+++ page/questionPage.java
@@ -26,4 +26,7 @@ public class questionPage extends JFrame {
         add(questionLabel);
         add(yesButton);
         add(noButton);        
+        
+        setVisible(true);
+    }        
 }
