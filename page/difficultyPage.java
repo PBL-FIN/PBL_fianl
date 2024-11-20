a95282ff4dd3c7e98371305946643f588e1f46b6
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-20 22:25:39 +0900

    Update difficultyPage.java

--- page/difficultyPage.java
+++ page/difficultyPage.java
@@ -16,4 +16,16 @@ public class difficultyPage extends JFrame {
         setSize(1980, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
+        
+        difficulty = new JLabel("게임 난이도");
+        normal = new JButton("보통");
+        normal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String difficulty = e.getActionCommand();
+                JOptionPane.showMessageDialog(null, difficulty + "으로 결정하겠습니까?");
+                dispose();
+                new hintPage();
+            }
+        });        
 }
