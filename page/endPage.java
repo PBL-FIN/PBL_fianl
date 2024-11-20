032596f8b44e3d24dec91d8240c492d55fd33013
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-20 22:24:07 +0900

    Update endPage.java

--- page/endPage.java
+++ page/endPage.java
@@ -1,1 +1,18 @@
+package page;
+
+import javax.swing.*;
+
+public class EndPage extends JFrame{
+    private JLabel result;
+    private JLabel score;
+    private JButton restart;
+    private JButton goToMenu;
+
+    public EndPage() {
+        setTitle("도형 넓이 구하는 게임!");
+        setSize(1980, 1080);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+    }
+}
 
