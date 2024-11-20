4ba24f8dc087d665d59e9c3b7fc2d2e1d8e09862
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-20 22:24:51 +0900

    Update difficultyPage.java

--- page/difficultyPage.java
+++ page/difficultyPage.java
@@ -1,1 +1,8 @@
+package page;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class difficultyPage extends JFrame {}
