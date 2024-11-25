662f1c8728490a6361f5a50b66f6da1cc0eb0400
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-25 20:57:07 +0900

    Update ShapeSelectionPage.java

--- page/ShapeSelectionPage.java
+++ page/ShapeSelectionPage.java
@@ -1,1 +1,14 @@
+package page;
 
+import page.description.*;
+import page.difficulty.difficultyPage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ShapeSelectionPage {
+    private JFrame frame;
+
+}
