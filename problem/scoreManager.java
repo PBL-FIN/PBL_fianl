a3301c35349a4fcd295741bf8221e29590d86449
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-25 20:25:04 +0900

    Update scoreManager.java

--- problem/scoreManager.java
+++ problem/scoreManager.java
@@ -1,1 +1,17 @@
+package problem;
 
+public class scoreManager {
+    private static int score = 0;
+
+    public void addScore(int points) {
+        score += points;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void resetScore() {
+        score = 0; // 점수 리셋
+    }
+}
