a32ee43f07e6c1e0ccb5ec42a9144dc89f136ba9
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-25 20:21:26 +0900

    Update normalDifficultyPage.java

--- difficulty/normalDifficultyPage.java
+++ difficulty/normalDifficultyPage.java
@@ -15,3 +15,14 @@ public class normalDifficultyPage extends JFrame {
     private String[] shapes = {"사각형", "삼각형"};
     private double answer;
     private String solutionProcess;
+    
+    public normalDifficultyPage(PageLoadingManager pageLoadingManager) {
+
+        setTitle("도형 넓이 구하는 게임!");
+        setSize(1980, 1080);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JLabel difficultyLabel = new JLabel("보통");
+        difficultyLabel.setBounds(20, 10, 100, 25);
+        add(difficultyLabel);
