4c24bd8257b26bc8d378645199d24ac9286f3517
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-11-25 20:24:28 +0900

    Update PageLoadingManager.java

--- problem/PageLoadingManager.java
+++ problem/PageLoadingManager.java
@@ -1,1 +1,13 @@
+package problem;
 
+public class PageLoadingManager {
+    private static int LoadingCnt = 0;
+
+    public void addCnt() {
+        LoadingCnt += 1;
+    }
+
+    public int getLoadingCnt() {
+        return LoadingCnt;
+    }
+}
