3c1d9c467689714224c7a5a3ea7bde11f2ce33bf
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-12-05 19:14:41 +0900

    Update difficultyPage.java

--- page/difficulty/difficultyPage.java
+++ page/difficulty/difficultyPage.java
@@ -2,6 +2,7 @@ package page.difficulty;
 
 import page.hintPage;
 import problem.PageLoadingManager;
+import problem.lifeManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +14,7 @@ public class difficultyPage extends JFrame {
     private JButton normal;
     private JButton easy;
     private PageLoadingManager pageLoadingManager = new PageLoadingManager();
+    private lifeManager lifeManager = new lifeManager();
 
     public difficultyPage() {
 
@@ -21,7 +23,14 @@ public class difficultyPage extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        difficulty = new JLabel("게임 난이도");
+        ImageIcon icon = new ImageIcon("img/ping2.png");
+        Image img = icon.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(img);
+        JLabel imageLabel = new JLabel(scaledIcon);
+        imageLabel.setBounds(1000, 50, 500, 500);
+        add(imageLabel);
+
+        difficulty = new JLabel("게임 난이도를 골라보소");
         normal = new JButton("보통");
         normal.addActionListener(new ActionListener() {
             @Override
@@ -30,7 +39,7 @@ public class difficultyPage extends JFrame {
                 int response = JOptionPane.showConfirmDialog(null, selectedDifficulty + "으로 결정하겠습니까?", "난이도 선택", JOptionPane.YES_NO_OPTION);
                 if (response == JOptionPane.YES_OPTION) {
                     dispose();
-                    new normalDifficultyPage(pageLoadingManager);
+                    new normalDifficultyPage(pageLoadingManager, lifeManager);
                 }
             }
         });
@@ -50,8 +59,8 @@ public class difficultyPage extends JFrame {
 
         difficulty.setFont(new Font(null, Font.PLAIN, 50));
         difficulty.setBounds(200, 400, 1000, 50);
-        normal.setBounds(500, 500, 100, 100);
-        easy.setBounds(700, 700, 200, 200);
+        normal.setBounds(500, 500, 150, 100);
+        easy.setBounds(300, 500, 150, 100);
         add(difficulty);
         add(normal);
         add(easy);
@@ -60,5 +69,4 @@ public class difficultyPage extends JFrame {
         setVisible(true);
     }
 }
-
 
